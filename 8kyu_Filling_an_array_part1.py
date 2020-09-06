61be8bba9fedfb25d3e73a9c1edb83ee3a6f06c1
Jarmi <70643136+jarmi95@users.noreply.github.com>
2020-09-06 09:58:10 +0200

    Create 8kyu_Filling_an_array_part1.py

---
+++ 8kyu_Filling_an_array_part1.py
@@ -0,0 +1,12 @@
+def arr(n=0):
+    cadena = []
+    if not n:
+        n == 0
+        cadena = []
+    else:
+        for i in range(0,n):
+            cadena.append(i)
+    return cadena
+
+#def arr(n=0): 
+    #return list(range(n))
